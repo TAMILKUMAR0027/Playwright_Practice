ce27bc4eb2cb37376e65bb1a873e4bfcdbd3b7ad
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-06-25 17:03:59 +0530

    Completed 5 testcases

---
+++ Automation_Practice/tests/Question6.test.ts
@@ -0,0 +1,19 @@
+import { chromium,test,expect } from "@playwright/test";
+
+test("Login test", async()=>{
+    const browser=await chromium.launch({
+        headless:false
+    })
+    const context= await browser.newContext();
+    const page=await context.newPage();
+    await page.goto("https://automationexercise.com/");
+    await page.getByRole('link', { name: ' Contact us' }).click();
+    await page.getByRole('textbox', { name: 'Name' }).fill("Tamil");
+    await page.getByRole('textbox', { name: 'Email', exact: true }).fill("tamilkumar0027@gmail.com");
+    await page.getByRole('textbox', { name: 'Subject' }).fill("Nothing");
+    await page.getByRole('textbox', { name: 'Your Message Here' }).fill("Good working execellent");
+    await page.getByRole('button', { name: 'Choose File' }).setInputFiles("C:\\Users\\tamil\\Downloads\\1. Sequence.pdf");
+    await page.getByRole('button', { name: 'Submit' }).click()
+    const msg=await page.locator("//div[@class='status alert alert-success']").textContent();
+    await expect("Success! Your details have been submitted successfully.").toBe(msg);
+});
